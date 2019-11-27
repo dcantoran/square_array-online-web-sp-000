de889d0082ce8a8623b23a1b6e7579a3ef65aa62
David Cantoran <davidcantoran36@gmail.com>
2019-11-27 15:53:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   # your code here
   squared_nums = []
-  array.each{|element| squared_nums <<  element ** element}
+  array.each{|num| squared_nums <<  num ** num}
   squared_nums
 end
